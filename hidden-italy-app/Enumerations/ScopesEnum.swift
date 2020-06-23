ebff93f659e0e73f9b31c4b97dbc110cbae03d04
danieletulone <danieletulone.work@gmail.com>
2020-06-23 19:20:21 +0200

    Add ScopesEnum

--- hidden-italy-app/Enumerations/ScopesEnum.swift
+++ hidden-italy-app/Enumerations/ScopesEnum.swift
@@ -7,3 +7,207 @@
 //
 
 import Foundation
+
+/**
+ * Enumeration that contains all default scopes.
+ *
+ * @author Daniele Tulone <danieletulone.work@gmail.com>
+ */
+enum ScopesEnum: String {
+    
+    /**
+     * Allow to create categories.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case createCategories
+    
+    /**
+     * Allow to create comments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case createComments
+    
+    /**
+     * Allow to create monuments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case createMonuments
+    
+    /**
+     * Allow to create roles.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case createRoles
+    
+    /**
+     * Allow to crete scopes.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case createScopes
+    
+    /**
+     * Allow to read categories.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case readCategories
+    
+    /**
+     * Allow to read comments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case readComments
+    
+    /**
+     * Allow to read monuments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case readMonuments
+    
+    /**
+     * Allow to create roles.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case readRoles
+    
+    /**
+     * Allow to create scopes.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case readScopes
+    
+    /**
+     * Allow to update categories.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case updateCategories
+    
+    /**
+     * Allow to update comments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case updateComments
+    
+    /**
+     * Allow to update monuments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case updateMonuments
+    
+    /**
+     * Allow to update roles.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case updateRoles
+    
+    /**
+     * Allow to update scopes.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case updateScopes
+    
+    /**
+     * Allow to delete categories.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case deleteCategories
+    
+    /**
+     * Allow to delete comments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case deleteComments
+    
+    /**
+     * Allow to delete monuments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case deleteMonuments
+    
+    /**
+     * Allow to delete roles.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case deleteRoles
+    
+    /**
+     * Allow to delete scopes.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case deleteScopes
+    
+    /**
+     * Allow to create, read, update and delete categories.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case manageCategories
+    
+    /**
+     * Allow to create, read, update and delete comments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case manageComments
+    
+    /**
+     * Allow to create, read, update and delete monuments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case manageMonuments
+    
+    /**
+     * Allow to create, read, update and delete roles.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case manageRoles
+    
+    /**
+     * Allow to create, read, update and delete scopes.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case manageScopes
+    
+    /**
+     * Allow to use admin dashbaord.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case useAdminDashboard
+    
+    /**
+     * Allow to suggests monuments.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case suggestsMonuments
+    
+    /**
+     * Allow to report users.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
+    case reportUsers
+}
