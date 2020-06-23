709954da0d98cb37c18ba0ab1fa1e2bad683a941
danieletulone <danieletulone.work@gmail.com>
2020-06-23 19:23:13 +0200

    Add user as environment object and format lines

--- hidden-italy-app/SceneDelegate.swift
+++ hidden-italy-app/SceneDelegate.swift
@@ -17,14 +17,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ManagerView()
+                
         
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            
+            window.rootViewController = UIHostingController(
+                rootView: ManagerView()
+                    .environmentObject(UserViewModel()
+                )
+            )
+            
             self.window = window
             window.makeKeyAndVisible()
         }
