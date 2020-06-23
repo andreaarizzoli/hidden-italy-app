68ff894a233c023dfdb06f83fbf5fa4f1620719e
danieletulone <danieletulone.work@gmail.com>
2020-06-23 18:43:46 +0200

    Add RegisterBody and add docstring in LoginBody

--- hidden-italy-app/Helpers/Api/Bodies/Auth/LoginBody.swift
+++ hidden-italy-app/Helpers/Api/Bodies/Auth/LoginBody.swift
@@ -8,7 +8,24 @@
 
 import Foundation
 
+/**
+ * Body for obtain a token and user data.
+ *
+ * @author Daniele Tulone <danieletulone.work@gmail.com>
+ */
 struct LoginBody: Codable {
+    
+    /**
+     * The email to send. Validation only back side.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
     var email: String
+    
+    /**
+     * The password to send.
+     *
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     */
     var password: String
 }
