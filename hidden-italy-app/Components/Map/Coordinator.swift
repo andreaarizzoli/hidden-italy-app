183d5f66c84e9f2b18ee6c08d454fe05d7efe544
danieletulone <danieletulone.work@gmail.com>
2020-06-24 14:54:33 +0200

    Add button and callback for open monument

--- hidden-italy-app/Components/Map/Coordinator.swift
+++ hidden-italy-app/Components/Map/Coordinator.swift
@@ -16,6 +16,35 @@ final class Coordinator: NSObject, MKMapViewDelegate{
         self.control = control
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+          if let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "bla") {
+              annotationView.annotation = annotation
+              return annotationView
+          } else {
+                let annotationView = MKPinAnnotationView(annotation:annotation, reuseIdentifier: "bla")
+                annotationView.isEnabled = true
+                annotationView.canShowCallout = true
+            
+                let btn = UIButton(type: .infoLight)
+                
+                annotationView.rightCalloutAccessoryView = btn
+            
+                return annotationView
+          }
+    }
+    
+    // When user taps on the disclosure button you can perform a segue to navigate to another view controller
+    func mapView(_ mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+
+        if control == view.rightCalloutAccessoryView{
+            let c = view.annotation as! CustomCoordinate
+            c.callback()
+            
+            //Perform a segue here to navigate to another viewcontroller
+            // On tapping the disclosure button you will get here
+        }
+    }
+    
     func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
         if let annotationView = views.first{
             if let annotation = annotationView.annotation {
