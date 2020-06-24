e39ce8a50f9f701881504125cb7120fabc76df29
andreaarizzoli <andrea.arizzoli@ied.edu>
2020-06-25 00:34:14 +0200

    Add image card and fix profileview

--- hidden-italy-app/Views/Profile/ProfileView.swift
+++ hidden-italy-app/Views/Profile/ProfileView.swift
@@ -22,89 +22,96 @@ struct ProfileView: View {
     
     var body: some View {
         
-        VStack (spacing:20){
-            HStack (alignment: .center){
-                Spacer()
-                VStack(alignment: .leading){
-                    Button(action: {
-                        self.showSheetSettingsView.toggle()
-                    }) {
-                        Image(systemName: "gear")
-                            .foregroundColor(Color(Accent))
-                            .font(.system(size: 24))
-                    }.sheet(isPresented: $showSheetSettingsView ) {
-                        UserSettings(
-                            showSheetSettingsView:
-                            self.$showSheetSettingsView
-                        ).environmentObject(self.user)
-                    }
-                }
-                
-            }.padding(.horizontal, 30)
-             .padding(.top, 15)
-             .modifier(PaddingSafeArea())
-            
-            HStack(spacing:20){
-                Button(action: {
-                    self.isShowingImagePicker.toggle()
-                }){
-                    ZStack (alignment: .center){
-                        
-                        if (!self.imageLoaded) {
-                            
-                            Image(uiImage: image)
-                                .resizable()
-                                .frame(width:125, height:125)
-                                .clipShape(RoundedRectangle(cornerRadius: 35))
-                                .modifier(AddImage())
-                            
-                            Image(systemName: "plus")
-                                .font(.system(size: 35))
+        ScrollView{
+            VStack (spacing:20){
+                HStack (alignment: .center){
+                    Spacer()
+                    VStack(alignment: .leading){
+                        Button(action: {
+                            self.showSheetSettingsView.toggle()
+                        }) {
+                            Image(systemName: "gear")
                                 .foregroundColor(Color(Accent))
+                                .font(.system(size: 24))
+                        }.sheet(isPresented: $showSheetSettingsView ) {
+                            UserSettings(
+                                showSheetSettingsView:
+                                self.$showSheetSettingsView
+                            ).environmentObject(self.user)
+                        }
+                    }
+                    
+                }.padding(.horizontal, 30)
+                 .padding(.top, 15)
+                 .modifier(PaddingSafeArea())
+                
+                HStack(spacing:20){
+                    Button(action: {
+                        self.isShowingImagePicker.toggle()
+                    }){
+                        ZStack (alignment: .center){
                             
-                        } else {
-                            if (self.user.image != nil) {
-                                URLImage(
-                                    URL(string: baseImageURL() + self.user.image!)!,
-                                    content: {
-                                        $0.image
-                                            .resizable()
-                                            .frame(width:125, height:125)
-                                            .clipShape(RoundedRectangle(cornerRadius: 35))
-                                            .modifier(AddImage())
-                                    }
-                                )
+                            if (!self.imageLoaded) {
+                                
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .frame(width:125, height:125)
+                                    .clipShape(RoundedRectangle(cornerRadius: 35))
+                                    .modifier(AddImage())
+                                
+                                Image(systemName: "plus")
+                                    .font(.system(size: 35))
+                                    .foregroundColor(Color(Accent))
+                                
+                            } else {
+                                if (self.user.image != nil) {
+                                    URLImage(
+                                        URL(string: baseImageURL() + self.user.image!)!,
+                                        content: {
+                                            $0.image
+                                                .resizable()
+                                                .frame(width:125, height:125)
+                                                .clipShape(RoundedRectangle(cornerRadius: 35))
+                                                .modifier(AddImage())
+                                        }
+                                    )
+                                }
+                                
                             }
                             
                         }
-                        
-                    }
-                }.buttonStyle(PlainButtonStyle())
-                    .sheet(isPresented: $isShowingImagePicker,
-                           content: {
-                            ImagePickerView(
-                                isPresented: self.$isShowingImagePicker,
-                                selectedImage: self.$image,
-                                newImage: self.$newImage,
-                                imageLoaded: self.$imageLoaded
-                            )
-                        })
+                    }.buttonStyle(PlainButtonStyle())
+                        .sheet(isPresented: $isShowingImagePicker,
+                               content: {
+                                ImagePickerView(
+                                    isPresented: self.$isShowingImagePicker,
+                                    selectedImage: self.$image,
+                                    newImage: self.$newImage,
+                                    imageLoaded: self.$imageLoaded
+                                )
+                            })
 
-                
-                Text("Ciao, \(user.current.firstname)").modifier(FormTextFieldText())
-                    .font(.system(size: 24))
-                    .padding(.bottom)
-            }.onAppear(perform: {
-                self.user.getCurrent(callback: {
-                    if (self.user.image != nil) {
-                        self.imageLoaded.toggle()
-                    }
+                    
+                    Text("Ciao, \(user.current.firstname)").modifier(FormTextFieldText())
+                        .font(.system(size: 24))
+                        .padding(.bottom)
+                }.onAppear(perform: {
+                    self.user.getCurrent(callback: {
+                        if (self.user.image != nil) {
+                            self.imageLoaded.toggle()
+                        }
+                    })
                 })
-            })
-            
-            UserTab()
-            
+//                Text("Monumenti visitati")
+//                .font(.largeTitle)
+//                .fontWeight(.medium)
+//                .foregroundColor(Color(Accent))
+//                SimpleFlip()
+                
+            }
         }.modifier(PaddingSafeArea()).modifier(BgSafearea())
+//            .onAppear { self.user.visitedMonument() }
+
     }
 }
 
